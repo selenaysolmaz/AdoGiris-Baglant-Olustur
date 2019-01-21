ec75d6401383d16df355eed01ceaa27012302e1a
semvs <semvs@ayvansaray.edu.tr>
2019-01-21 11:05:40 +0300

    baglanti olustur

---
+++ AdoGiris/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace AdoGiris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnOpenClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(65, 49);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(65, 95);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnOpenClose
+            // 
+            this.btnOpenClose.Location = new System.Drawing.Point(65, 151);
+            this.btnOpenClose.Name = "btnOpenClose";
+            this.btnOpenClose.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenClose.TabIndex = 0;
+            this.btnOpenClose.Text = "open/close";
+            this.btnOpenClose.UseVisualStyleBackColor = true;
+            this.btnOpenClose.Click += new System.EventHandler(this.btnOpenClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(231, 238);
+            this.Controls.Add(this.btnOpenClose);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnOpenClose;
+    }
+}
+
---
+++ AdoGiris/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+
+namespace AdoGiris
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                con = new SqlConnection(ConfigurationManager.ConnectionStrings["Baglanti"].ConnectionString);
+            }
+            catch(System.ApplicationException ex)
+            {
+                Console.WriteLine("Error reading from {0}.Message= {1}", ex.Source, ex.Message);
+            }
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            //bağlantı oluşturmak,açmak için.
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+                MessageBox.Show("baglantı acıldı.");
+            }
+            else
+            {
+                MessageBox.Show("açık bir bağlantı var.");
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+                MessageBox.Show("bağlantı kapatıldı.");
+
+            }
+            else
+            {
+                MessageBox.Show("bağlantı açık değil.");
+            }
+        }
+
+        private void btnOpenClose_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                    MessageBox.Show("baglantı acıldı.");
+                }
+                else
+                {
+                    con.Close();
+                    MessageBox.Show("bağlantı kapatıldı.");
+                }
+            }
+            catch(Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+    }
+}
